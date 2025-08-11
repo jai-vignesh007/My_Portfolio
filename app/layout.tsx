a4a999b394ab9b80d19fea2829acf9d88b3ee669
jai-vignesh007 <ravichandran.ja@northeastern.edu>
2025-08-11 10:45:28 -0400

    Inital commit Next.js

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import {Inter } from "next/font/google";
+import "./globals.css";
+
+const font=Inter({
+  weight:['100','200','300','400','500','600','700','800','900'],
+  subsets:['latin'],
+}
+)
+
+export const metadata: Metadata = {
+  title: "Jai Vignesh Ravichandran",
+  description: "Personal Portfolio using Next js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${font.className} antialiased bg-[#0d0d1f]`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
